2ee9048af8a2178b94372fd83e1fe421a9bda28c
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-02-10 08:59:35 +0200

    Fatal error is fixed but customization on buttons does not work because of the same fatal error. Trying to implement if let statements into ALL customization objects

---
+++ camshare/camshare/CreateAccountViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateAccountViewController.swift
+//  camshare
+//
+//  Created by Janco Erasmus on 2020/02/07.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: ViewController {
+    
+//    //MARK: CREATE ACCOUNT VIEW OBJECTS
+//    @IBOutlet weak var createAccountUsingLabel: UILabel!
+//    @IBOutlet weak var facebookButton: UIButton!
+//    @IBOutlet weak var instagramButton: UIButton!
+//    @IBOutlet weak var googleButton: UIButton!
+//    @IBOutlet weak var emailButton: UIButton!
+//    @IBOutlet weak var alreadyHaveAnAccountButton: UIButton!
+//
+//    //MARK: CREATE ACCOUNT VIEW OBJECT CONSTRAINTS
+//    @IBOutlet weak var faceBookButtonTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var instagramButtonTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var googleButtonTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var emailButtonTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var alreadyHaveAnAccountTopConstraint: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+}
---
+++ camshare/camshare/PhotoAlbumViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoAlbumViewController.swift
+//  camshare
+//
+//  Created by Janco Erasmus on 2020/02/07.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: ViewController {
+
+    //MARK: OUTLETS
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    //MARK: DATASOURCE
+
+}
+
+//MARK: DATASOURCE
+
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ _collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
+        return 10
+    }
+    
+}
