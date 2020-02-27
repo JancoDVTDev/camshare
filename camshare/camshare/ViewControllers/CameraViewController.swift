8100c8e0573fdaab28a45473eb97b0242269bbf3
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-02-27 08:14:14 +0200

    Forgot to create new branch

---
+++ camshare/camshare/ViewControllers/CameraViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CameraViewController.swift
+//  camshare
+//
+//  Created by Janco Erasmus on 2020/02/26.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var takenPhoto: UIImage?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let availableImage = takenPhoto {
+            imageView.image = availableImage
+        }
+    }
+    
+    @IBAction func goBackButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
