e66a52d724eaa98354568db07ed214ab9e8f0fdb
Janco Erasmus <60605366+JancoDVTDev@users.noreply.github.com>
2020-03-16 06:47:34 +0200

    Feature/sign up login user view model testing cs (#23)
    * Functions tested
    
    * Some Unit tests done

--- camshare/camshareTests/ShowingAllUserAlbumsViewModelTest.swift
+++ camshare/camshareTests/ShowingAllUserAlbumsViewModelTest.swift
@@ -2,7 +2,7 @@
 //  ShowingAllUserAlbumsViewModelTest.swift
 //  camshareTests
 //
-//  Created by Janco Erasmus on 2020/03/08.
+//  Created by Janco Erasmus on 2020/03/09.
 //  Copyright © 2020 DVT. All rights reserved.
 //
 
@@ -11,13 +11,24 @@ import XCTest
 @testable import camshare
 
 class ShowingAllUserAlbumsViewModelTest: XCTestCase {
+
     var systemUnderTest: ShowingAllUserAlbumsViewModel!
-    override class func setUp() {
-        //systemUnderTest = ShowingAllUserAlbumsViewModel()
+    override func setUp() {
+        systemUnderTest = ShowingAllUserAlbumsViewModel()
     }
-    
-    override func tearDown() {
-        
+
+    func testGivenANewAlbumThenGenerateAUniqiueAlbumID() {
+        let uniqueID = systemUnderTest.generateUniqueAlbumID()
+        var isCreated: Bool = false
+        if uniqueID.count == 25 {
+            isCreated = true
+        }
+        XCTAssert(isCreated)
     }
-    
+
+    func testGivenUniqueAlbumIDExistsWhenGeneratingNewAlbumID() {
+        let uniqueID = "1"
+        let result = systemUnderTest.isIDUnique(generatedID: uniqueID)
+        XCTAssert(result)
+    }
 }
