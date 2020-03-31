61f7f197e0898ce780c1854855be8c65b2703ad0
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-03-31 08:53:19 +0200

    Login UITests

--- camshare/camshareUITests/Scenarios/LoginUser.swift
+++ camshare/camshareUITests/Scenarios/LoginUser.swift
@@ -10,18 +10,104 @@ import XCTest
 
 class LoginUser: camshareTestCase {
 
-//    func testUserLogin() {
-//        application.launch()
-//        environment.setUpWith(username: "au@test.com", password: "Pass1234!")
+    var app: XCUIApplication!
+
+    override func setUp() {
+        app = XCUIApplication()
+        continueAfterFailure = false
+    }
+
+    func testGivenUserLoginWhenCredentialsAreCorrectThenLoginSuccesful() {
+        app.launch()
+
+        app.buttons["Login"].tap()
+        sleep(1)
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("urtt@test.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("Pass1234!")
+        app.buttons["Login"].tap()
+        sleep(3)
+        
+        XCTAssert(app.navigationBars["My Albums"].buttons["Add"].exists)
+    }
+
+    func testGivenUserLoginWhenCredentialsAreIncorrectEmailThenLoginUnsuccesful() {
+//        app.launch()
 //
-//        application.buttons["Login"].tap()
-//        completeUserLoginInDetails()
-//        application.buttons["Login"].tap()
-//        sleep(2)
-//        XCTAssert(application.navigationBars["My Albums"].buttons["Add"].exists)
-//    }
-//
-//    func testRecord() {
-//
-//    }
+//        app.buttons["Login"].tap()
+//        sleep(1)
+//        app.textFields["Email"].tap()
+//        app.textFields["Email"].typeText("urtttest.com")
+//        app.secureTextFields["Password"].tap()
+//        app.secureTextFields["Password"].typeText("Pass1234!")
+//        app.buttons["Login"].tap()
+//        sleep(3)
+//        
+//        XCTAssert(app.staticTexts["Email not valid format"].exists)
+    }
+
+    func testGivenUserLoginWhenCredentialsAreIncorrectPasswordLengthThenLoginUnsuccesful() {
+        app.launch()
+
+        app.buttons["Login"].tap()
+        sleep(1)
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("urtt@test.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("Pass")
+        app.buttons["Login"].tap()
+        sleep(3)
+
+        XCTAssert(app.staticTexts["Password at least 8 characters"].exists)
+    }
+
+    func testGivenUserLoginWhenCredentialsAreIncorrectPasswordSpecialThenLoginUnsuccesful() {
+        app.launch()
+
+        app.buttons["Login"].tap()
+        sleep(1)
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("urtt@test.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("Pass1234")
+        app.buttons["Login"].tap()
+        sleep(3)
+
+        XCTAssert(app.staticTexts["No SPECIAL CHARACTER"].exists)
+    }
+
+    func testGivenUserLoginWhenCredentialsAreIncorrectPasswordCapitalThenLoginUnsuccesful() {
+        app.launch()
+
+        app.buttons["Login"].tap()
+        sleep(1)
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("urtt@test.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("p@ss1234")
+        app.buttons["Login"].tap()
+        sleep(3)
+
+        XCTAssert(app.staticTexts["No CAPITAL LETTER"].exists)
+    }
+
+    func testGivenUserLoginWhenCredentialsAreIncorrectPasswordNumberThenLoginUnsuccesful() {
+        app.launch()
+
+        app.buttons["Login"].tap()
+        sleep(1)
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("urtt@test.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("P@ssword")
+        app.buttons["Login"].tap()
+        sleep(3)
+
+        XCTAssert(app.staticTexts["No NUMBER"].exists)
+    }
+
+    func testRecord() {
+
+    }
 }
