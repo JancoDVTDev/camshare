1b0676c7ddd776eaf54746e8247fb9d80209c1de
Janco Erasmus <jerasmus@jhb.dvt.co.za>
2020-04-18 11:13:55 +0200

    UI Changes and Rating functionality

--- camshare/camshare/ViewControllers/TipsAndTricsViewController.swift
+++ camshare/camshare/ViewControllers/TipsAndTricsViewController.swift
@@ -11,56 +11,49 @@ import camPod
 
 class TipsAndTricsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var tipsTricsTableView: UITableView!
     var tableView: UITableView!
     var tipsAndTricksContent = [TipsAndTricksModel]()
+    var selectedSegment = 0
+
+    var tipsAndTricksViewModel = TipsAndTricsViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = false
 
-        tableView = UITableView(frame: view.bounds, style: .plain)
-        view.addSubview(tableView)
+        tipsAndTricksViewModel.view = self
+        tipsAndTricksViewModel.getRepo = camshareAPIGet()
 
-        let celNib = UINib(nibName: "TipsAndTricsTableViewCell", bundle: nil)
-        tableView.register(celNib, forCellReuseIdentifier: "tipsAndTricsCell")
+        configureTableView()
 
-        var layoutGuide: UILayoutGuide!
-        layoutGuide = view.safeAreaLayoutGuide
+        tipsAndTricksViewModel.loadTipsAndTricks()
 
-        tableView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
-
-        tableView.delegate = self
-        tableView.dataSource = self
-
-        let viewModel = TipsAndTricsViewModel()
-        viewModel.getTipsTricks(request: "tipsandtricks") { (result) in
-            self.tipsAndTricksContent = result
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
+//        let viewModel = TipsAndTricsViewModel()
+//        viewModel.getTipsTricks(request: "tipsandtricks") { (result) in
+//            self.tipsAndTricksContent = result
+//            DispatchQueue.main.async {
+//                self.tableView.reloadData()
+//            }
+//        }
     }
 
-    @objc func statusChanged(_ sender: UISegmentedControl!) {
-        let viewModel = TipsAndTricsViewModel()
-        let status = sender.selectedSegmentIndex
-        if status == 0 {
-            viewModel.changeStatus(status: "updatestatusnew",
-                                   model: tipsAndTricksContent[sender.tag]) { (model) in
-                                    self.tipsAndTricksContent[sender.tag] = model
-                                    self.tableView.reloadData()
-            }
+    @IBAction func segmentedControlValueChanged(segment: UISegmentedControl) {
+        if segment.selectedSegmentIndex == 0 {
+            // Cards
+            print("Cards")
+            selectedSegment = 0
+            tableView.reloadData()
         }
 
-        if status == 1 {
-            viewModel.changeStatus(status: "updatestatustried",
-                                   model: tipsAndTricksContent[sender.tag]) { (model) in
-                                    self.tipsAndTricksContent[sender.tag] = model
-                                    self.tableView.reloadData()
-            }
+        if segment.selectedSegmentIndex == 1 {
+            // Videos
+            print("Videos")
+            selectedSegment = 1
+            tableView.reloadData()
         }
     }
 
@@ -76,11 +69,168 @@ class TipsAndTricsViewController: UIViewController, UITableViewDelegate, UITable
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tipsAndTricsCell", for: indexPath) as! TipsAndTricsTableViewCell
     //swiftlint:enable all
-        cell.headingLabel.text = tipsAndTricksContent[indexPath.item].heading
-        cell.bodyTextView.text = tipsAndTricksContent[indexPath.item].body
-        cell.statusSegmentControl.tag = indexPath.row
-        cell.statusSegmentControl.addTarget(self, action: #selector(statusChanged), for: .valueChanged)
+
+        if selectedSegment == 0 {
+            cell.cellView.isHidden = false
+            cell.headingLabel.text = tipsAndTricksContent[indexPath.item].heading
+            cell.bodyTextView.text = tipsAndTricksContent[indexPath.item].body
+
+            let currentRatingTap = UITapGestureRecognizer(target: self, action: #selector(currentRatingTapped(_:)))
+            cell.currentRatingImageView.isUserInteractionEnabled = true
+            cell.currentRatingImageView.tag = indexPath.row
+            cell.currentRatingImageView.addGestureRecognizer(currentRatingTap)
+        }
+
+        if selectedSegment == 1 {
+            cell.cellView.isHidden = true
+        }
+
         return cell
     }
-    
+
+    @IBAction func currentRatingTapped(_ sender: AnyObject) {
+        print("Tap Gesture for Cell Works: tapped - \(sender.view.tag)")
+        let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.animateRatings()
+
+            let veryBadRatingTap = UITapGestureRecognizer(target: self, action: #selector(veryBadRatingTapped(_:)))
+            let badRatingTap = UITapGestureRecognizer(target: self, action: #selector(badRatingTapped(_:)))
+            let okayRatingTap = UITapGestureRecognizer(target: self, action: #selector(okayRatingTapped(_:)))
+            let goodRatingTap = UITapGestureRecognizer(target: self, action: #selector(goodRatingTapped(_:)))
+            let veryGoodRatingTap = UITapGestureRecognizer(target: self, action: #selector(veryGoodRatingTapped(_:)))
+            currentCell.veryBadRatingImageView.addGestureRecognizer(veryBadRatingTap)
+            currentCell.badRatingImageView.addGestureRecognizer(badRatingTap)
+            currentCell.okayRatingImageView.addGestureRecognizer(okayRatingTap)
+            currentCell.goodRatingImageView.addGestureRecognizer(goodRatingTap)
+            currentCell.veryGoodRatingImageView.addGestureRecognizer(veryGoodRatingTap)
+            currentCell.veryBadRatingImageView.tag = indexPath.row
+            currentCell.badRatingImageView.tag = indexPath.row
+            currentCell.okayRatingImageView.tag = indexPath.row
+            currentCell.goodRatingImageView.tag = indexPath.row
+            currentCell.veryGoodRatingImageView.tag = indexPath.row
+            currentCell.veryBadRatingImageView.isUserInteractionEnabled = true
+            currentCell.badRatingImageView.isUserInteractionEnabled = true
+            currentCell.okayRatingImageView.isUserInteractionEnabled = true
+            currentCell.goodRatingImageView.isUserInteractionEnabled = true
+            currentCell.veryGoodRatingImageView.isUserInteractionEnabled = true
+        }
+    }
+
+    @IBAction func veryBadRatingTapped(_ sender: AnyObject) {
+        let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.currentRatingImageView.image = UIImage(named: "very bad")
+            currentCell.animateRatings()
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func badRatingTapped(_ sender: AnyObject) {
+                let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.currentRatingImageView.image = UIImage(named: "bad")
+            currentCell.animateRatings()
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func okayRatingTapped(_ sender: AnyObject) {
+        let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.currentRatingImageView.image = UIImage(named: "okay")
+            currentCell.animateRatings()
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func goodRatingTapped(_ sender: AnyObject) {
+        let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.currentRatingImageView.image = UIImage(named: "good")
+            currentCell.animateRatings()
+            tableView.reloadData()
+        }
+    }
+
+    @IBAction func veryGoodRatingTapped(_ sender: AnyObject) {
+        let indexPath = NSIndexPath(row: sender.view.tag, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+            //swiftlint:disable all
+            let currentCell = cell as! TipsAndTricsTableViewCell
+            //swiftlint:enable all
+            currentCell.currentRatingImageView.image = UIImage(named: "very good")
+            currentCell.animateRatings()
+            tableView.reloadData()
+        }
+    }
+
+    func configureTableView() {
+        // View Components
+        let segemtedControl = UISegmentedControl(items: ["Cards", "Videos"])
+        segemtedControl.selectedSegmentIndex = 0
+        segemtedControl.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        segemtedControl.selectedSegmentTintColor = UIColor(red: 24/255, green: 172/255, blue: 251/255, alpha: 1)
+        segemtedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.addSubview(tableView)
+        let celNib = UINib(nibName: "TipsAndTricsTableViewCell", bundle: nil)
+        tableView.register(celNib, forCellReuseIdentifier: "tipsAndTricsCell")
+        tableView.tableHeaderView = segemtedControl
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        tableView.isHidden = true
+
+        // Contraints
+        var layoutGuide: UILayoutGuide!
+        layoutGuide = view.safeAreaLayoutGuide
+
+        tableView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: segemtedControl.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
+
+        // Delegate and datasource injection
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+extension TipsAndTricsViewController: TipsAndTricksViewType {
+    public func updateTableViewCardsSource(content: [TipsAndTricksModel]) {
+        tipsAndTricksContent = content
+    }
+
+    public func didFinishLoading() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.isHidden = true
+            self.tableView.isHidden = false
+        }
+    }
+
+    public func displayError(error: String) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    public func reloadTableView() {
+        DispatchQueue.main.async { // Correct
+            self.tableView.reloadData()
+        }
+    }
 }
--- camshare/camshare/Views/TipsAndTricsTableViewCell.swift
+++ camshare/camshare/Views/TipsAndTricsTableViewCell.swift
@@ -10,18 +10,91 @@ import UIKit
 import camPod
 
 class TipsAndTricsTableViewCell: UITableViewCell {
+    @IBOutlet var cellView: UIView!
     @IBOutlet var headingLabel: UILabel!
     @IBOutlet var bodyTextView: UITextView!
-    @IBOutlet var statusSegmentControl: UISegmentedControl!
+    @IBOutlet var currentRatingImageView: UIImageView!
+    @IBOutlet var veryBadRatingImageView: UIImageView!
+    @IBOutlet var badRatingImageView: UIImageView!
+    @IBOutlet var okayRatingImageView: UIImageView!
+    @IBOutlet var goodRatingImageView: UIImageView!
+    @IBOutlet var veryGoodRatingImageView: UIImageView!
 
+    @IBOutlet var currentLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet var verybadLeadingContraint: NSLayoutConstraint!
+    @IBOutlet var badLeadingContraint: NSLayoutConstraint!
+    @IBOutlet var okayLeadingContraint: NSLayoutConstraint!
+    @IBOutlet var goodLeadingContraint: NSLayoutConstraint!
+    @IBOutlet var veryGoodLeadingContraint: NSLayoutConstraint!
+    var didTapCell = false
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        cellView.layer.cornerRadius = 4
+        cellView.layer.shadowColor = UIColor.darkGray.cgColor
+        cellView.layer.shadowOpacity = 0.75
+        cellView.layer.shadowRadius = 2
+        cellView.layer.shadowOffset = .zero
+
+        verybadLeadingContraint.constant -= self.bounds.width
+        badLeadingContraint.constant -= self.bounds.width
+        okayLeadingContraint.constant -= self.bounds.width
+        goodLeadingContraint.constant -= self.bounds.width
+        veryGoodLeadingContraint.constant -= self.bounds.width
+
     }
 
-    @IBAction func statusChangedSegementControl(_ sender: Any) {
-
+    func animateRatings() {
+        didTapCell = !didTapCell
+        if didTapCell {
+            showRatings()
+            ratingsSlideIntoView()
+        } else {
+            ratingSlideOutOfView()
+            //hideRatings()
+        }
     }
+
+    private func showRatings() {
+        veryBadRatingImageView.isHidden = false
+        badRatingImageView.isHidden = false
+        okayRatingImageView.isHidden = false
+        goodRatingImageView.isHidden = false
+        veryGoodRatingImageView.isHidden = false
+    }
+
+    private func ratingsSlideIntoView() {
+        verybadLeadingContraint.constant = currentLeadingConstraint.constant
+        badLeadingContraint.constant = verybadLeadingContraint.constant
+        okayLeadingContraint.constant = badLeadingContraint.constant
+        goodLeadingContraint.constant = okayLeadingContraint.constant
+        veryGoodLeadingContraint.constant = goodLeadingContraint.constant
+
+        UIView.animate(withDuration: 0.5) {
+            self.layoutIfNeeded()
+        }
+    }
+
+    private func ratingSlideOutOfView() {
+        verybadLeadingContraint.constant -= self.bounds.width
+        badLeadingContraint.constant -= self.bounds.width
+        okayLeadingContraint.constant -= self.bounds.width
+        goodLeadingContraint.constant -= self.bounds.width
+        veryGoodLeadingContraint.constant -= self.bounds.width
+
+        UIView.animate(withDuration: 0.5) {
+            self.layoutIfNeeded()
+        }
+    }
+
+    private func hideRatings() {
+        veryBadRatingImageView.isHidden = true
+        badRatingImageView.isHidden = true
+        okayRatingImageView.isHidden = true
+        goodRatingImageView.isHidden = true
+        veryGoodRatingImageView.isHidden = true
+    }
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
