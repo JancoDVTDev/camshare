d8fea57745d20a6be124ef8d553b716872c9c5ab
Janco Erasmus <60605366+JancoDVTDev@users.noreply.github.com>
2020-04-20 19:07:41 +0200

    Image FileManager added (#55)
    * Image FileManager added
    
    * Codacy Fix 1

--- camshare/camshare/ViewControllers/PhotoAlbumViewController.swift
+++ camshare/camshare/ViewControllers/PhotoAlbumViewController.swift
@@ -121,6 +121,15 @@ class PhotoAlbumViewController: ViewController, AVCaptureMetadataOutputObjectsDe
             self.captureQRCode()
         }
 
+        let signOut = UIAlertAction(title: "Sign Out", style: .destructive) { (_) in
+            do {
+                try Auth.auth().signOut()
+                exit(0)
+            } catch {
+                print("Error Signing out")
+            }
+        }
+
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
             self.trackAnalytics.log(name: NameConstants.cancelAdd, parameters: nil)
         }
@@ -128,6 +137,7 @@ class PhotoAlbumViewController: ViewController, AVCaptureMetadataOutputObjectsDe
         actionSheet.addAction(createNewAction)
         actionSheet.addAction(scanQRCode)
         actionSheet.addAction(existingAlbumAction)
+        actionSheet.addAction(signOut)
         actionSheet.addAction(cancelAction)
         self.present(actionSheet, animated: true, completion: nil)
     }
