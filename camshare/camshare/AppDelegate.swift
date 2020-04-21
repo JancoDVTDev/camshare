98ca5956211b7d1803fb18b26dd5ce12e2416c28
Janco Erasmus <60605366+JancoDVTDev@users.noreply.github.com>
2020-04-21 20:54:38 +0200

    Core Data implemented (#56)
    * Core Data implemented
    
    * Bitrise check nor recognised

--- camshare/camshare/AppDelegate.swift
+++ camshare/camshare/AppDelegate.swift
@@ -13,9 +13,11 @@ UIImage(named: "Image10"), UIImage(named: "Image11"), UIImage(named: "Image12")]
 
 import UIKit
 import Firebase
+import CoreData
+import camPod
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, SavedImagesType {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -40,5 +42,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after
         // application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: Core data required functions
+    lazy var persistentContainer: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "SavedImages")
+      container.loadPersistentStores(completionHandler: { (_, error) in
+        if let error = error as NSError? {
+          fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+      })
+      return container
+    }()
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let error = error as NSError
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+
+    func returnContainer() -> NSPersistentContainer {
+        return persistentContainer
     }
 }
---
+++ camshare/camshare/Coredata/Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  
+//
+//  Created by Janco Erasmus on 2020/04/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
